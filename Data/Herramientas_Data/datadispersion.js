ab2b8357d2f1063c4511023c30b3ac9ef18f053a
MyoRap <geragran@espol.edu.ec>
2023-01-08 12:44:26 -0500

    datos para examenes

--- Data/Herramientas_Data/datadispersion.js
+++ Data/Herramientas_Data/datadispersion.js
@@ -10,4 +10,60 @@
   "intensidad": 10
    }
 
+{
+  "x":100,
+  "y":30,
+  "intensidad": 4
+  }
+ ,{
+   "x":50,
+  "y":450,
+  "intensidad": 2
+   }
+
+{
+  "x":1,
+  "y":3,
+  "intensidad": 6
+  }
+ ,{
+   "x":400,
+  "y":100,
+  "intensidad": 8
+   }
+{
+  "x":800,
+  "y":100,
+  "intensidad": 2
+  }
+ ,{
+   "x":502,
+  "y":455,
+  "intensidad": 10
+   }
+   
+   
+   {
+  "x":125,
+  "y":35,
+  "intensidad": 3
+  }
+ ,{
+   "x":256,
+  "y":780,
+  "intensidad": 7
+   }
+   
+   {
+  "x":14,
+  "y":13,
+  "intensidad": 7
+  }
+ ,{
+   "x":325,
+  "y":123,
+  "intensidad": 10
+   }
+
+
 ]
